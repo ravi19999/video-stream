b936ce0680f6bdb0666e3687239a19939cb24e31
Ravi <dahalravindra@gmail.com>
2021-03-03 21:30:51 +0545

    Created routes for all of the components :sparkles:

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,30 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">pageOne</Link>
-    </div>
-  );
-};
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo <button>Click Me!</button>
-      <Link to="/">Pageone</Link>
-    </div>
-  );
-};
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne}></Route>
-          <Route path="/pagetwo" exact component={PageTwo}></Route>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
