c00ff457bd7ea67b4540e4040580cc29b0dc4755
Ravi <dahalravindra@gmail.com>
2021-03-06 13:53:06 +0545

    Added event handlers to the buttons :sparkles:'

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -22,13 +22,21 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
         <div>
-          <button className="ui red google button">
+          <button onClick={this.onSignOut} className="ui red google button">
             <i className="google icon" />
             Sign Out
           </button>
@@ -36,9 +44,11 @@ class GoogleAuth extends React.Component {
       );
     } else {
       return (
-        <div className="ui blue google button">
-          <i className="google icon" />
-          Sign In with Google
+        <div>
+          <button onClick={this.onSignIn} className="ui blue google button">
+            <i className="google icon" />
+            Sign In with Google
+          </button>
         </div>
       );
     }
