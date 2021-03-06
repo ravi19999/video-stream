a8880481c9453b1605536fbe6745102ed436a3c2
Ravi <dahalravindra@gmail.com>
2021-03-06 14:32:19 +0545

    Did an initial redux setup :boom:

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
 
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
@@ -18,15 +20,19 @@ class GoogleAuth extends React.Component {
     });
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut;
+    }
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -36,7 +42,10 @@ class GoogleAuth extends React.Component {
     } else if (this.state.isSignedIn) {
       return (
         <div>
-          <button onClick={this.onSignOut} className="ui red google button">
+          <button
+            onClick={this.onSignOutClick}
+            className="ui red google button"
+          >
             <i className="google icon" />
             Sign Out
           </button>
@@ -45,7 +54,10 @@ class GoogleAuth extends React.Component {
     } else {
       return (
         <div>
-          <button onClick={this.onSignIn} className="ui blue google button">
+          <button
+            onClick={this.onSignInClick}
+            className="ui blue google button"
+          >
             <i className="google icon" />
             Sign In with Google
           </button>
@@ -57,4 +69,4 @@ class GoogleAuth extends React.Component {
     return <div>{this.renderAuthButton()}</div>;
   }
 }
-export default GoogleAuth;
+export default connect(null, { signIn, signOut })(GoogleAuth);
