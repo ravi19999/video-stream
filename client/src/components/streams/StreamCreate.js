908c3bdebdf3e9c82a1c490c19c8f70a332d7e1f
Ravi <dahalravindra@gmail.com>
2021-03-15 15:33:27 +0545

    Created action for creating streams :sparkles:

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError = ({ error, touched }) => {
@@ -23,9 +25,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -56,7 +58,10 @@ const validate = (formValues) => {
   }
   return errors;
 };
-export default reduxForm({
+
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
